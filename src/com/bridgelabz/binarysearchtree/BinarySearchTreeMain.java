3d4184fb460c76f2a816124b088cc30f167a16ca
srinadh1304 <srinadh1304@gmail.com>
2021-09-02 07:35:53 +0530

    ADDED:Welcome Message

---
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.binarysearchtree;
+
+public class BinarySearchTreeMain {
+	public static void main(String args[]) {
+		System.out.println("******Welcome to Binary Search Tree Program **********");
+	}
+}
