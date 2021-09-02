876fdfacb1a26b8542d6c28d17491dac53b448f8
srinadh1304 <srinadh1304@gmail.com>
2021-09-02 07:45:36 +0530

    MODIFIED:calling size method

--- src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
@@ -7,8 +7,21 @@ public class BinarySearchTreeMain {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(25);
+		myBinaryTree.add(20);
+		myBinaryTree.add(51);
+		myBinaryTree.add(37);
+		myBinaryTree.add(1);
+		myBinaryTree.add(7);
+		myBinaryTree.add(40);
+		myBinaryTree.add(35);
+		myBinaryTree.add(57);
+		myBinaryTree.add(69);
+		myBinaryTree.add(67);
 		
-		System.out.println("Inorder traversal : ");
+		System.out.println("inorder traversal is:");
 		myBinaryTree.inorderTraversal();
+		System.out.println();
+		System.out.println("the size of tree is :"+ myBinaryTree.getSize());
 	}
 }
