bdc1ece50a3a51c080327e2076f066839cb165b0
srinadh1304 <srinadh1304@gmail.com>
2021-09-02 07:37:13 +0530

    ADDED:Class containg main method

--- src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
@@ -3,5 +3,12 @@ package com.bridgelabz.binarysearchtree;
 public class BinarySearchTreeMain {
 	public static void main(String args[]) {
 		System.out.println("******Welcome to Binary Search Tree Program **********");
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		
+		System.out.println("inorder traversal is:");
+		myBinaryTree.inorderTraversal();
 	}
 }
